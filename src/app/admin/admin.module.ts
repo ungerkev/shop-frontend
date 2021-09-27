59ecab34abf94c2c595a323af6113f8a9550fe01
Kevin Unger <unger.kevin97@gmail.com>
2021-09-27 20:07:45 +0200

    admin login component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { adminRouting } from './admin-routing';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     adminRouting,
